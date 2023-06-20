a8a25bf2408e5bd707b6a894dbc32ef39c7332cd
veesesh <vee1912566@gmail.com>
2023-06-20 22:06:13 +0530

    Groups and chat history

--- chat_backend/index.js
+++ chat_backend/index.js
@@ -1,74 +1,4 @@
-// // Importing redis client
-// // By default the redis client connects to redis instance running at localhost:6379
-// const Redis = require("ioredis");
-// const redisClient = new Redis();
-
-// // Importing socket.io, http and express
-// // and initializing express and socket.io
-// const { Server } = require("socket.io");
-// const express = require("express");
-// const http = require("http");
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// // Importing Joi and uuid
-// const { v4: uuidv4 } = require("uuid");
-// const Joi = require("joi");
-
-// // Defining a basic schema to validate
-// // incoming chat messages
-// const chatMessageSchema = Joi.object({
-//   username: Joi.string().min(3).max(30).required(),
-//   message: Joi.string().min(1).max(1000).required(),
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("kuch bhi");
-// });
-
-// io.on("connection", async (socket) => {
-//   console.log("user connected");
-
-//   const existingMessages = await redisClient.lrange("chat_messages", 0, -1);
-//   const parsedMessages = existingMessages.map((item) => JSON.parse(item));
-//   socket.emit("messages", parsedMessages);
-
-//   socket.on("message", (data) => {
-//     console.log(data);
-//     // Validating the message
-//     const { value, error } = chatMessageSchema.validate(data);
-
-//     // If the message is invalid, then sending error
-//     if (error) {
-//       console.log("Invalid message, error occurred", error);
-//       // Triggering an error event to the user
-//       socket.emit("error", error);
-//       return;
-//     }
-
-//     // If message is valid, then creating a new message object
-//     const newMessage = {
-//       id: uuidv4(), // Generating a unique id for the message
-//       username: value.username,
-//       message: value.message,
-//       created: new Date().getTime(), // Creating timestamp for the message
-//     };
-
-//     // Saving message in redis in list named "chat_messages"
-//     redisClient.lpush("chat_messages", JSON.stringify(newMessage));
-
-//     // Sending the new message to call the connected clients
-//     io.emit("message", newMessage);
-//   });
-// });
-
-// server.listen(3000, () => {
-//   console.log("App started on port 3000");
-// });
-
-
-// Importing redis client
+//Importing redis client
 // By default the redis client connects to redis instance running at localhost:6379
 const Redis = require("ioredis");
 const redisClient = new Redis();
@@ -91,7 +21,11 @@ const Joi = require("joi");
 const chatMessageSchema = Joi.object({
   username: Joi.string().min(3).max(30).required(),
   message: Joi.string().min(1).max(1000).required(),
+  groupId: Joi.string().required(),
 });
+
+// Store chat messages for each group
+const groupMessages = {};
 
 app.get("/", (req, res) => {
   res.send("kuch bhi");
@@ -100,11 +34,20 @@ app.get("/", (req, res) => {
 io.on("connection", async (socket) => {
   console.log("user connected");
 
-  const existingMessages = await redisClient.lrange("chat_messages", 0, -1);
-  const parsedMessages = existingMessages.map((item) => JSON.parse(item));
-  socket.emit("messages", parsedMessages);
+  socket.on("joinGroup", async (groupId) => {
+    socket.join(groupId);
 
-  socket.on("message", (data) => {
+    if (!groupMessages[groupId]) {
+      const existingMessages = await redisClient.lrange(`chat_messages:${groupId}`, 0, -1);
+      const parsedMessages = existingMessages.map((item) => JSON.parse(item));
+      groupMessages[groupId] = parsedMessages;
+    }
+
+    const existingMessages = groupMessages[groupId];
+    socket.emit("messages", { messages: existingMessages });
+  });
+
+  socket.on("message", async (data) => {
     console.log(data);
     // Validating the message
     const { value, error } = chatMessageSchema.validate(data);
@@ -125,15 +68,20 @@ io.on("connection", async (socket) => {
       created: new Date().getTime(), // Creating timestamp for the message
     };
 
-    // Saving message in redis in list named "chat_messages"
-    redisClient.lpush("chat_messages", JSON.stringify(newMessage));
+    // Saving message in Redis for the corresponding group
+    const groupId = value.groupId;
+    if (!groupMessages[groupId]) {
+      groupMessages[groupId] = [];
+    }
+    groupMessages[groupId].push(newMessage);
+    await redisClient.lpush(`chat_messages:${groupId}`, JSON.stringify(newMessage));
 
-    // Sending the new message to all the connected clients
-    io.emit("message", newMessage);
+    // Sending the new message to all the connected clients in the group
+    io.to(groupId).emit("message", newMessage);
   });
-
 });
 
 server.listen(3000, () => {
   console.log("App started on port 3000");
 });
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+
 void main() {
   runApp(GroupChatApp());
 }
+
 class GroupChatApp extends StatefulWidget {
   @override
   _GroupChatAppState createState() => _GroupChatAppState();
 }
+
 class _GroupChatAppState extends State<GroupChatApp> {
   final TextEditingController _messageController = TextEditingController();
   final List<String> _messages = [];
   late IO.Socket socket;
+  String _selectedGroup = 'group1'; // Default selected group
+
   @override
   void initState() {
     super.initState();
-    socket = IO.io('https://21df-49-37-222-123.ngrok-free.app/', <String, dynamic>{
+    socket = IO.io('http://localhost:3000/', <String, dynamic>{
       'transports': ['websocket'],
     });
     socket.onConnect((_) {
       print('Connected to server');
+      socket.emit('joinGroup', _selectedGroup); // Join the default group
+    });
+    socket.on('messages', (data) {
+      print(data);
+      List<dynamic> messages = data['messages'];
+      setState(() {
+        _messages.clear();
+        messages.forEach((message) {
+          _messages.add("${message['username']}: ${message['message']}");
+        });
+      });
     });
     socket.on('message', (data) {
       print(data);
       setState(() {
         _messages.add("${data['username']}: ${data['message']}");
       });
-      print(_messages);
     });
   }
+
   void _sendMessage() {
     final message = _messageController.text;
     final username = "ritvik"; // Set the desired username here
     if (message.isNotEmpty) {
-      socket.emit('message', {'username': username, 'message': message});
+      socket.emit('message', {
+        'username': username,
+        'message': message,
+        'groupId': _selectedGroup,
+      });
       _messageController.clear();
     }
   }
+
+  void _changeGroup(String groupId) {
+    setState(() {
+      _selectedGroup = groupId;
+      _messages.clear();
+    });
+    socket.emit('joinGroup', groupId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Group Chat',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Group Chat'),
+          title: Row(
+            children: [
+              Icon(Icons.group),
+              SizedBox(width: 8),
+              Text('Group: $_selectedGroup'),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Groups',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Group 1'),
+                onTap: () => _changeGroup('group1'),
+              ),
+              ListTile(
+                title: Text('Group 2'),
+                onTap: () => _changeGroup('group2'),
+              ),
+              ListTile(
+                title: Text('Group 3'),
+                onTap: () => _changeGroup('group3'),
+              ),
+            ],
+          ),
         ),
         body: Column(
           children: [
-            // Expanded(
-            //   child: ListView.builder(
-            //     itemCount: _messages.length,
-            //     itemBuilder: (context, index) {
-            //       return ListTile(
-            //         title: Text(_messages[index]),
-            //       );
-            //     },
-            //   ),
-            // ),
             Expanded(
               child: ListView.builder(
                 itemCount: _messages.length,
                 itemBuilder: (context, index) {
-                  String username="";
-                  String messageText="";
+                  String username = "";
+                  String messageText = "";
 
-                  if(_messages[index].isEmpty)
-                    return Container(  );
-                  else if(_messages.length >=1){
-                    final message = _messages[index].split(':');
-                    username = message[0].trim();
-                    messageText = message[1].trim();
-                  }
+                  if (_messages[index].isEmpty) return Container();
+                  final message = _messages[index].split(':');
+                  username = message[0].trim();
+                  messageText = message[1].trim();
 
                   return Card(
                     child: ListTile(
@@ -80,7 +133,6 @@ class _GroupChatAppState extends State<GroupChatApp> {
                 },
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
@@ -106,6 +158,7 @@ class _GroupChatAppState extends State<GroupChatApp> {
       ),
     );
   }
+
   @override
   void dispose() {
     socket.dispose();
